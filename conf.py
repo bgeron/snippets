5c6d6ed3a6e201268cc9e9a3740e565b54268b29
Bram Geron <bgeron@bgeron.nl>
2015-05-11 13:02:51 +0100

    Add a GitHub banner

--- conf.py
+++ conf.py
@@ -109,7 +109,11 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 # html_theme_options = {'collapsiblesidebar': True}
-html_theme_options = {}
+html_theme_options = {
+	'github_user': 'bgeron',
+	'github_repo': 'snippets',
+	'github_banner': True
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
